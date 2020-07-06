f5d0415defea5592dd620c05c14ab2835387d0cf
杨统兵 <1280758415@qq.com>
2020-07-06 17:09:37 +0800

    init

---
+++ src/ComposerTest2.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: osx
+ * Date: 2020/7/6
+ * Time: 15:38
+ */
+namespace Sdwgyzyxy;
+
+class ComposerTest2
+{
+    private static $instance;
+
+    public function __construct()
+    {
+        var_dump('123123123');
+    }
+
+    public function config()
+    {
+        return ['composer-test2'];
+    }
+
+    public static function getInstance()
+    {
+        if (!isset(self::$instance) || self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+}
